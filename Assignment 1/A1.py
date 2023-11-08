69cada9b6d4591a179719d8ae38df64f10b9de70
Severyn "Seiven" Balaniuk <sbalaniu@uwaterloo.ca>
2023-11-08 18:10:35 -0500

    Defining the program. Creating skeleton of the program

--- Assignment 1/A1.py
+++ Assignment 1/A1.py
@@ -1,27 +1,96 @@
-import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
+import argparse
+import json
+import random
 
-def generate_binary_numbers(max_digits=8):
-    return np.random.randint(0, 2, max_digits).tolist()
+# Set device for PyTorch
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def one_hot_encode(binary_number):
-    # Assuming binary_number is a list of integers (0 or 1)
-    return np.eye(2)[binary_number]
-
-def generate_dataset(train_size, test_size, seed):
+# 1. Data Generation
+def generate_data(train_size, test_size, seed):
+    """
+    This function generates pairs of binary integers and their products.
+    """
     random.seed(seed)
-    training_data = []
-    testing_data = []
-    for _ in range(train_size + test_size):
-        a = generate_binary_numbers()
-        b = generate_binary_numbers()
-        a_int = int(''.join(str(x) for x in reversed(a)), 2)
-        b_int = int(''.join(str(x) for x in reversed(b)), 2)
-        product = a_int * b_int
-        c = [int(x) for x in reversed(bin(product)[2:].zfill(16))]
-        data_point = (one_hot_encode(a), one_hot_encode(b), one_hot_encode(c))
-        if len(training_data) < train_size:
-            training_data.append(data_point)
-        else:
-            testing_data.append(data_point)
-    return training_data, testing_data
+    # Implement data generation logic here
+    # ...
+
+# 2. RNN Model Creation
+class BinaryMultiplicationRNN(nn.Module):
+    """
+    This class defines the RNN model for binary multiplication.
+    """
+    def __init__(self, input_size, hidden_size, output_size):
+        super(BinaryMultiplicationRNN, self).__init__()
+        # Define the RNN layers and output layer
+        # ...
+
+    def forward(self, input):
+        # Implement the forward pass
+        # ...
+
+# 3. Training
+def train(model, train_data, optim_params, model_params):
+    """
+    This function trains the RNN model.
+    """
+    # Set up the loss function, optimizer, and other training components
+    # ...
+
+# 4. Evaluation
+def evaluate(model, test_data):
+    """
+    This function evaluates the RNN model on test data.
+    """
+    # Implement the evaluation logic here
+    # ...
+
+# 5. Loss Calculation with Swapped Inputs
+def loss_with_swapped_inputs(model, data):
+    """
+    This function computes the loss with swapped inputs.
+    """
+    # Implement the logic to swap inputs and compute loss
+    # ...
+
+# 6. Argument Parsing
+def parse_arguments():
+    """
+    This function parses command-line arguments.
+    """
+    parser = argparse.ArgumentParser(description='Train an RNN for binary integer multiplication')
+    parser.add_argument('--param', type=str, help='file containing hyperparameters', required=True)
+    parser.add_argument('--train-size', type=int, help='size of the generated training set', required=True)
+    parser.add_argument('--test-size', type=int, help='size of the generated test set', required=True)
+    parser.add_argument('--seed', type=int, help='random seed used for creating the datasets', required=True)
+    args = parser.parse_args()
+    return args
+
+# 7. Reporting and Main Function
+def main():
+    args = parse_arguments()
+    
+    # Load hyperparameters from the JSON file
+    with open(args.param, 'r') as f:
+        hyperparams = json.load(f)
+    
+    # Generate data
+    train_data = generate_data(args.train_size, args.test_size, args.seed)
+    test_data = generate_data(args.test_size, args.test_size, args.seed)
+    
+    # Initialize the model
+    model = BinaryMultiplicationRNN(...)
+    # Train the model
+    train(model, train_data, hyperparams['optim'], hyperparams['model'])
+    # Evaluate the model
+    evaluate(model, test_data)
+    # Calculate loss with swapped inputs
+    loss_with_swapped_inputs(model, test_data)
+    # Print out or log the results
+    # ...
+
+if __name__ == '__main__':
+    main()
