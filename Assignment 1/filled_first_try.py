dcb4b8fb9b3315cecae7352c0874747b08972bdc
Severyn "Seiven" Balaniuk <sbalaniu@uwaterloo.ca>
2023-11-16 11:07:42 -0500

    .\filled_first_try.py

--- Assignment 1/filled_first_try.py
+++ Assignment 1/filled_first_try.py
@@ -1,19 +1,16 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
 import numpy as np
+import random
+import torch
+import torch.optim as optim
+import matplotlib.pyplot as plt
 import argparse
 import json
-import random
-
-# Set device for PyTorch
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Function to convert integer to binary array
 def int_to_bin_array(num, num_bits):
     return [int(digit) for digit in bin(num)[2:].zfill(num_bits)][::-1]
 
-# 1. Data Generation
+# Data Generation
 def generate_data(size, num_bits, seed):
     np.random.seed(seed)
     max_num = 2**num_bits - 1
@@ -25,136 +22,188 @@ def generate_data(size, num_bits, seed):
         a_bin = int_to_bin_array(a, num_bits)
         b_bin = int_to_bin_array(b, num_bits)
         c_bin = int_to_bin_array(c, 2 * num_bits)
-        data.append((a_bin, b_bin, c_bin))
+        input_seq = []
+        for a_bit, b_bit in zip(a_bin, b_bin):
+            input_seq.extend([a_bit, b_bit])
+        #input_seq.append(0)  # Padding to match the output length
+        data.append((input_seq, c_bin))
+        #print(data)
     return data
 
-# 2. RNN Model Creation
+import torch.nn as nn
+
+# RNN Model
 class BinaryMultiplicationRNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, num_layers):
         super(BinaryMultiplicationRNN, self).__init__()
-        self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.hidden_size = hidden_size
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out)
+        h0 = torch.zeros(x.size(0), self.hidden_size).to(x.device)
+        out, _ = self.rnn(x)#, h0)
+        out = torch.sigmoid(self.fc(out))  # Get the output of the last time step
         return out
 
-# 3. Training
-def train(model, train_data, optimizer, criterion, num_epochs, batch_size):
+train_losses = []
+train_accuracies = []
+
+# Training Function
+def train(model, train_data, optimizer, criterion, num_epochs, batch_size, device, train_losses, train_accuracies):
     model.train()
     for epoch in range(num_epochs):
         random.shuffle(train_data)
         total_loss = 0
+        total_accuracy = 0
+
         for i in range(0, len(train_data), batch_size):
             batch = train_data[i:i + batch_size]
+            x_batch, y_batch = zip(*batch)
+            x_tensor = torch.tensor(x_batch, dtype=torch.float).to(device)
+            y_tensor = torch.tensor(y_batch, dtype=torch.float).to(device)
+
             optimizer.zero_grad()
-
-            for a, b, c in batch:
-                x = torch.tensor([[a_i, b_i] for a_i, b_i in zip(a, b)], dtype=torch.float).to(device)
-                y = torch.tensor(c, dtype=torch.float).to(device)
-
-                pred = model(x)
-                loss = criterion(pred.view(-1), y)
-                total_loss += loss.item()
-                loss.backward()
-
+            pred = model(x_tensor)
+            loss = criterion(pred, y_tensor)
+            loss.backward()
             optimizer.step()
-        
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {total_loss / len(train_data):.4f}')
 
-# 4. Evaluation
-def evaluate(model, test_data, criterion):
-    model.eval()
-    with torch.no_grad():
-        total_loss = 0
-        for a, b, c in test_data:
-            x = torch.tensor([[a_i, b_i] for a_i, b_i in zip(a, b)], dtype=torch.float).to(device)
-            y = torch.tensor(c, dtype=torch.float).to(device)
-            pred = model(x)
-            loss = criterion(pred.view(-1), y)
             total_loss += loss.item()
+            total_accuracy += binary_accuracy(pred, y_tensor)
+
+        epoch_loss = total_loss / len(train_data)
+        epoch_accuracy = total_accuracy / len(train_data)
+        
+    train_losses.append(epoch_loss)
+    train_accuracies.append(epoch_accuracy)
+
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.4f}')
+
+
+# Evaluation Function
+def evaluate(model, test_data, criterion, device):
+    model.eval()
+    total_loss = 0
+
+    with torch.no_grad():
+        for x, y in test_data:
+            x_tensor = torch.tensor([x], dtype=torch.float).to(device)
+            y_tensor = torch.tensor([y], dtype=torch.float).to(device)
+
+            pred = model(x_tensor)
+            loss = criterion(pred, y_tensor)
+            total_loss += loss.item()
+
     avg_loss = total_loss / len(test_data)
     return avg_loss
 
-# 5. Loss Calculation with Swapped Inputs
-def loss_with_swapped_inputs(model, data, criterion):
+# Loss Calculation with Swapped Inputs
+def loss_with_swapped_inputs(model, data, criterion, device):
     model.eval()
+    total_loss = 0
+
     with torch.no_grad():
-        total_loss = 0
-        for a, b, c in data:
-            x = torch.tensor([[b_i, a_i] for a_i, b_i in zip(a, b)], dtype=torch.float).to(device)
-            y = torch.tensor(c, dtype=torch.float).to(device)
-            pred = model(x)
-            loss = criterion(pred.view(-1), y)
+        for x, y in data:
+            swapped_x = [x[i+1] if i % 2 == 0 else x[i-1] for i in range(len(x)-1)] + [0]
+            x_tensor = torch.tensor([swapped_x], dtype=torch.float).to(device)
+            y_tensor = torch.tensor([y], dtype=torch.float).to(device)
+
+            pred = model(x_tensor)
+            loss = criterion(pred, y_tensor)
             total_loss += loss.item()
+
     avg_loss = total_loss / len(data)
     return avg_loss
+def binary_accuracy(y_pred, y_true):
+    # Applying sigmoid function to convert logits to probabilities
+    y_pred = torch.sigmoid(y_pred)
+    # Rounding to get binary predictions
+    predicted = y_pred.round()
+    # Calculating accuracy
+    correct = (predicted == y_true).float()
+    accuracy = correct.sum() / correct.numel()
+    return accuracy.item()
 
-# 6. Argument Parsing
-def parse_arguments():
+
+import json
+import argparse
+import torch
+
+def main():
+    # Argument Parsing
     parser = argparse.ArgumentParser(description='Train an RNN for binary integer multiplication')
     parser.add_argument('--param', type=str, help='file containing hyperparameters', required=True)
     parser.add_argument('--train-size', type=int, help='size of the generated training set', required=True)
     parser.add_argument('--test-size', type=int, help='size of the generated test set', required=True)
     parser.add_argument('--seed', type=int, help='random seed used for creating the datasets', required=True)
-    return parser.parse_args()
+    args = parser.parse_args()
 
-# 7. Reporting and Main Function
-def main():
-    args = parse_arguments()
-    
-    # Load hyperparameters from the JSON file
+    # Load Hyperparameters
     with open(args.param, 'r') as f:
         hyperparams = json.load(f)
-    
-    # Generate data
+
+    # Generate Data
     train_data = generate_data(args.train_size, hyperparams['data']['max_digits'], args.seed)
     test_data = generate_data(args.test_size, hyperparams['data']['max_digits'], args.seed)
-    
+
     # Initialize the model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model_config = hyperparams['model']
     model = BinaryMultiplicationRNN(
-        input_size=2, 
+        input_size=16, 
         hidden_size=model_config['rnn_units_per_layer'], 
-        output_size=1, 
+        output_size=16,  # 16 bits for 8-bit multiplication
         num_layers=model_config['rnn_layers']
     ).to(device)
 
-    # Training setup
-    training_config = hyperparams['training']
-    if model_config['optimizer'].lower() == 'adam':
-        optimizer = optim.Adam(model.parameters(), lr=model_config['learning_rate'])
+    training_config = hyperparams['training']  # This line gets the 'training' section
+
+    # Use training_config to access 'optimizer' and 'loss_function'
+    if training_config['optimizer'].lower() == 'adam':
+        optimizer = torch.optim.Adam(model.parameters(), lr=training_config['learning_rate'])
     else:
         raise ValueError("Unsupported optimizer type provided.")
 
     if training_config['loss_function'].lower() == 'bcewithlogitsloss':
-        criterion = nn.BCEWithLogitsLoss()
+        criterion = torch.nn.BCEWithLogitsLoss()
     else:
         raise ValueError("Unsupported loss function provided.")
+    
+    train_losses = []
+    train_accuracies = []
 
-    # Train the model
-    train(
-        model, 
-        train_data, 
-        optimizer, 
-        criterion, 
-        training_config['epochs'], 
-        training_config['batch_size']
-    )
+    # Train the Model
+    train(model, train_data, optimizer, criterion, hyperparams['training']['epochs'], 
+          hyperparams['training']['batch_size'], device, train_losses, train_accuracies)
 
-    # Evaluate the model
-    eval_loss = evaluate(model, test_data, criterion)
+    # Evaluate the Model
+    eval_loss = evaluate(model, test_data, criterion, device)
     print(f'Evaluation Loss: {eval_loss:.4f}')
 
-    # Calculate loss with swapped inputs
-    swap_loss = loss_with_swapped_inputs(model, test_data, criterion)
+    # Calculate Loss with Swapped Inputs
+    swap_loss = loss_with_swapped_inputs(model, test_data, criterion, device)
     print(f'Loss with Swapped Inputs: {swap_loss:.4f}')
+    
+    # Plot training loss
+    plt.figure(figsize=(10, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(train_losses, label='Training Loss')
+    plt.title('Training Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    # Plot training accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(train_accuracies, label='Training Accuracy')
+    plt.title('Training Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.show()
 
 if __name__ == '__main__':
     main()
-
